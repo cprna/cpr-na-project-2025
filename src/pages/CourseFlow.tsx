05041ddadce5a6bedd8c8cf5c461855e406d1b0d
ParkPassTH <Parkpassth@gmail.com>
2025-07-25 21:16:58 +0700

    Update CourseFlow.tsx

--- src/pages/CourseFlow.tsx
+++ src/pages/CourseFlow.tsx
@@ -428,7 +428,7 @@ const CourseFlow = () => {
                   </div>
                   <div className="flex items-center space-x-2">
                     <Clock className="w-5 h-5" />
-                    <span>รวม 50+ นาที</span>
+                    <span>รวม 10+ นาที</span>
                   </div>
                   <div className="flex items-center space-x-2">
                     <Users className="w-5 h-5" />
