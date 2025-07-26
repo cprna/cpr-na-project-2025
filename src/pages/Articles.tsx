b7c17f73e3aa24f317dbfa15ac2d00c533b94203
ParkPassTH <Parkpassth@gmail.com>
2025-07-26 23:19:20 +0700

    Update Articles.tsx

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -278,7 +278,7 @@ const Articles = () => {
       readTime: "1 นาที",
       content: <></>,
       icon: FileText,
-      imageUrl: "/src/assets/แผ่นพับ.pdf_page-0001.jpg"
+      imageUrl: "/public/แผ่นพับ.pdf_page-0001.jpg"
     },
     {
       id: "poster-aed",
@@ -289,7 +289,7 @@ const Articles = () => {
       readTime: "1 นาที",
       content: <></>,
       icon: FileText,
-      imageUrl: "/src/assets/แผ่นพับ.pdf_page-0002.jpg"
+      imageUrl: "/public/แผ่นพับ.pdf_page-0002.jpg"
     },
     {
       id: "basics",
