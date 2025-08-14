d11e409aad1b1f134ec4d82089b0c3eaa418fd23
cprna <66128301060@bcnkk.ac.th>
2025-08-14 23:42:06 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ import {
   CheckCircle
 } from "lucide-react";
 import Navbar from "@/components/Navbar";
-import heroImage from "@/assets/IMG_0110.jpg";
+import heroImage from "@/assets/IMG_7142.JPG";
 import { useState } from "react";
 import { useSimpleAuth } from "@/hooks/useSimpleAuth";
 import SimpleLogin from "@/components/SimpleLogin";
